4f5fea85ca6f743e860360747ac8947af16c0949
Andrii Zelenko <qvuer7@gmail.com>
2021-12-02 02:12:45 +0000

    pull

---
+++ game.py
@@ -0,0 +1,24 @@
+import numpy as np 
+
+def game() -> int:
+    number = np.random.randint(1,100)
+    
+    count = 0
+    
+    while True: 
+        count +=1 
+        
+        predicted = int(input('give me your guess ->'))
+        
+        if predicted > number : 
+            print("predicted is more then actual")
+        elif predicted < number: 
+            print("predicted is less then actual")
+        else: 
+            break
+    return count
+
+if __name__ == "__main__":
+    print(f'You gessed the number in {game()} attempts')
+
+    
---
+++ gamePC.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def predict_number(number:int, min:int, max:int) -> int:
+    """computer guess random number
+
+    Args:
+        number (int): number to guess
+        min (int): lower border of randomisation
+        max (int): highest border of randomisation
+
+    Returns:
+        int: number of attempts taken
+    """
+    
+    count = 0 
+    
+    while True:
+        count+=1
+        predicted = np.random.randint(min,max)
+        if predicted > number: 
+            max = predicted
+        elif predicted < number: 
+                min = predicted
+        else: 
+            break
+    return count
+
+def mean_attempts(predict_number) -> int: 
+    """calculate avarage number of steps taken to guess random number
+
+    Args:
+        predict_number ([type]): prediction function
+
+    Returns:
+        int: avarage number of guesses
+    """
+    
+    s = 0
+    for _ in range(1,100):
+        number = np.random.randint(1,100)
+        s += predict_number(number, 1,100)
+        
+    return s/100
+
+if __name__ == '__main__':
+    print(f'Avrage number of steps taken for laptop to guess the number = {mean_attempts(predict_number)}')
